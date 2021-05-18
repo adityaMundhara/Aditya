494870ee0eb71223fd40f4ca2d16331d78ce3428
Aditya Mundhara <mundhraaditya3@gmail.com>
2021-05-18 19:44:27 +0530

    new icon

--- docs/polyfills.js
+++ docs/polyfills.js
@@ -7,7 +7,7 @@
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! /Users/adityamundhara/Desktop/personal/me/src/polyfills.ts */"hN/g");
+module.exports = __webpack_require__(/*! E:\Projects\me\src\polyfills.ts */"hN/g");
 
 
 /***/ }),
